0c2ca78563cd1f37b6e03520e1294fe4a15a8cc6
Atinderpal Singh <atindermann08@gmail.com>
2015-07-13 23:01:24 +0530

    errors partial extracted

---
+++ resources/views/layouts/partials/_errors.blade.php
@@ -0,0 +1,28 @@
+@if (count($errors) > 0)
+  <div class="alert alert-danger">
+    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+    <ul>
+      @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
+@endif
+
+@if(Session::has('message'))
+<div class="alert alert-warning alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  {{ Session::get('message') }}
+</div>
+@endif
+@if(Session::has('error'))
+  <div class="alert alert-warning alert-dismissible" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    {{Session::get('error')}}
+    {{--
+  	@foreach(Session::get('error') as $error)
+      {{ $error }}
+  	@endforeach
+    --}}
+  </div>
+@endif
