42e8a9d892ec8a9c2a7b72e931c7caee92d6d355
Atinderpal Singh <atindermann08@gmail.com>
2015-07-20 12:28:38 +0530

    book club controller miderlware auth

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -10,6 +10,12 @@ use App\Http\Controllers\Controller;
 
 class BookController extends Controller
 {
+
+
+    public function __construct(){
+      $this->middleware('auth', ['except' => ['index' , 'show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
