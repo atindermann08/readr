c4522d4da6c841a7e879cf45c59639034b060a5b
Atinderpal Singh <atindermann08@gmail.com>
2015-07-23 21:33:49 +0530

    book view extracted in partial

---
+++ resources/views/partials/_showbook.blade.php
@@ -0,0 +1,27 @@
+<div class="media">
+  <div class="media-left">
+    <a href="{{route('books.show',$book->id)}}">
+        <i class='fa fa-leanpub fa-4x book-default-pic'></i>
+    </a>
+  </div>
+  <div class="media-body">
+    <h3 class="media-heading">{!! link_to_route('books.show',$book->title,$book->id) !!}</h3>
+    <small>
+      by:
+        @foreach($book->authors as $key => $author)
+            @unless($key == 0)
+              ,
+            @endunless
+            {{$author->name}}
+        @endforeach
+    </small>
+    <p>Description: {{$book->description}}</p>
+    <small>Language: {{$book->language->name}},</small>
+    <small>Category: {{$book->category->name}},</small>
+    <small>Status:
+      @foreach($statuses as $count=>$status)
+        {{ $status }} {{$count}}
+      @endforeach
+    </small>
+  </div>
+</div>
