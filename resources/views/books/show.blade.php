cb55b99844fb3c96f055c62b8692be10cf5f2bf4
Atinderpal Singh <atindermann08@gmail.com>
2015-07-23 22:21:12 +0530

    request button removed temporarily from book views

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.default')
 
 @section('content')
-  @include('partials._showbook')@unless(false)
+  @include('partials._showbook')
+  {{--
+  @unless(false)
     <p>{!! link_to_route('books.request','Request Book', $book->id, ['class'=>'btn btn-primary']) !!}</p>
   @else
     <p class=''>Request Pending</p>
   @endunless
+  --}}
   <hr/>
 @stop()
