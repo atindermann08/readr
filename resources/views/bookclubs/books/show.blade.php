a4cf26f38e6e349d7b86f7dffe4833bfa04e6b81
Atinderpal Singh <atindermann08@gmail.com>
2015-07-29 23:30:01 +0530

    request visibility fixed for non mebers

--- resources/views/bookclubs/books/show.blade.php
+++ resources/views/bookclubs/books/show.blade.php
@@ -1,17 +1,22 @@
 @extends('layouts.default')
 
 @section('content')
+
+@include('partials._showbookclub')
+  <hr/>
   @include('partials._showbook')
   <hr/>
   <ul class="list-group">
     @foreach($book->owners as $owner)
       <li class="list-group-item">
         @unless($owner->id == auth()->user()->id)
-          <span class="pull-right">
-            <a href="{{ route('bookclubs.books.requestbook', [$bookclub->id, $book->id]) }}" class=''>
-              Request Book
-            </a>
-          </span>
+          @if(auth()->user()->isMember($bookclub->id))
+            <span class="pull-right">
+              <a href="{{ route('bookclubs.books.requestbook', [$bookclub->id, $book->id]) }}" class=''>
+                Request Book
+              </a>
+            </span>
+          @endif
         @endunless
         {{ $owner->name }} ({{ $owner->email }})
       </li>
