492665b5fbee38497c8bee6b89b2ac2f119ad65c
Atinderpal Singh <atindermann08@gmail.com>
2015-07-30 10:35:09 +0530

    migrations for book request bookclubjoin request and book from book club request added

---
+++ database/migrations/2015_07_30_050048_create_book_club_join_requests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookClubJoinRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_club_join_requests', function (Blueprint $table) {
+            $table->integer('book_club_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('book_club_join_requests');
+    }
+}
---
+++ database/migrations/2015_07_30_050213_create_requests_book_club_books_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequestsBookClubBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request_book_club_books', function (Blueprint $table) {
+            $table->integer('book_club_id')->unsigned();
+            $table->integer('book_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('request_book_club_books');
+    }
+}
