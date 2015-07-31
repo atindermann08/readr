33155a749ca8fd55bd97322187009e5437a4c445
Atinderpal Singh <atindermann08@gmail.com>
2015-07-31 23:03:02 +0530

    check for availablity added in notification view

--- resources/views/notifications.blade.php
+++ resources/views/notifications.blade.php
@@ -4,19 +4,23 @@
     <h3>Notifications</h3>
     <hr/>
     <h4>Book Club Joining requests</h4>
-    <ul class="list-group">
-      @foreach($requests as $request)
-        <li class="list-group-item">
-          <span class="pull-right">
-            <a href="{{ route('bookclubs.requests.accept', $request->id) }}" class='btn'>
-              Accept
-            </a>
-            <a href="{{ route('bookclubs.requests.reject', $request->id) }}" class='btn'>
-              Reject
-            </a>
-          </span>
-          {{ $request->bookclub->name }} Joining Request <small>by:{{ $request->requestee->name }} ({{ $request->requestee->email }})</small>
-        </li>
-      @endforeach
-    </ul>
+    @if($requests->count())
+      <ul class="list-group">
+        @foreach($requests as $request)
+          <li class="list-group-item">
+            <span class="pull-right">
+              <a href="{{ route('bookclubs.requests.accept', $request->id) }}" class='btn'>
+                Accept
+              </a>
+              <a href="{{ route('bookclubs.requests.reject', $request->id) }}" class='btn'>
+                Reject
+              </a>
+            </span>
+            {{ $request->bookclub->name }} Joining Request <small>by:{{ $request->requestee->name }} ({{ $request->requestee->email }})</small>
+          </li>
+        @endforeach
+      </ul>
+    @else
+      <p>No book club joining requests</p>
+    @endif
 @stop()
