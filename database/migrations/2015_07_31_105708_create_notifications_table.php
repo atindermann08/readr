1bbd27bb1a3620717cb6562be9eb65808898931c
Atinderpal Singh <atindermann08@gmail.com>
2015-07-31 16:33:06 +0530

    notification migration added

---
+++ database/migrations/2015_07_31_105708_create_notifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('type');
+            $table->integer('count');
+            $table->string('text');
+            $table->string('url');
+            $table->boolean('is_read');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('notifications');
+    }
+}
