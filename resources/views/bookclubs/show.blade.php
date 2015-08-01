c73e729683fd90475e1b5d523317a3df70193792
Atinderpal Singh <atindermann08@gmail.com>
2015-08-01 22:02:38 +0530

    bookclub view check for member to show book add form

--- resources/views/bookclubs/show.blade.php
+++ resources/views/bookclubs/show.blade.php
@@ -3,20 +3,22 @@
 @section('content')
     @include('partials._showbookclub')
       <hr/>
-      <div class="row">
-        {!! Form::open(['route' => ['bookclubs.books.store', $bookclub->id]]) !!}
-        <div class="col-md-6">
-          <div class="form-group">
-            {!! Form::select('bookIds[]', $books ,null, ['class' => 'form-control dropdown book-title-select-book-club' , 'multiple']) !!}
+      @if(auth()->user()->isMember($bookclub->id))
+        <div class="row">
+          {!! Form::open(['route' => ['bookclubs.books.store', $bookclub->id]]) !!}
+          <div class="col-md-6">
+            <div class="form-group">
+              {!! Form::select('bookIds[]', $books ,null, ['class' => 'form-control dropdown book-title-select-book-club' , 'multiple']) !!}
+            </div>
           </div>
-        </div>
-        <div class="col-md-6">
-          <div class="form-group">
-            {!! Form::submit('Add Books', ['class' => 'btn btn-primary']) !!}
+          <div class="col-md-6">
+            <div class="form-group">
+              {!! Form::submit('Add Books', ['class' => 'btn btn-primary']) !!}
+            </div>
           </div>
+          {!! Form::close() !!}
         </div>
-        {!! Form::close() !!}
-      </div>
+      @endif
       @foreach ($bookclub->books->unique()->chunk(2) as $chunk)
           <div class="row">
             @foreach ($chunk as $book)
