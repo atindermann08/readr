9d342c517e9feb6008f3395b948dd3563758fcb0
Atinderpal Singh <atindermann08@gmail.com>
2015-08-02 21:31:41 +0530

    active nitification color changed

--- resources/views/layouts/partials/_nav.blade.php
+++ resources/views/layouts/partials/_nav.blade.php
@@ -54,7 +54,7 @@
         @if(Auth::check())
           <li class="dropdown">
             <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-              <i class=' fa fa-bell fa-fw'></i>
+              <i class='fa fa-bell fa-fw @if(count($notifications)) notifications-active @endif'></i>
             </a>
             <ul class="dropdown-menu">
               @if(count($notifications))
