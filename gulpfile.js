88d63779dbb8043f6565e51356a94b3e0129b5d5
Atinderpal Singh <atindermann08@gmail.com>
2015-08-02 14:43:33 +0530

    public assets moved in public/assets

--- gulpfile.js
+++ gulpfile.js
@@ -12,22 +12,22 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss')
-      .copy("bower_components/font-awesome/css/font-awesome.min.css", "public/vendor/css/font-awesome.min.css")
-      .copy("bower_components/font-awesome/fonts/fontawesome-webfont.eot", "public/vendor/fonts/fontawesome-webfont.eot")
-      .copy("bower_components/font-awesome/fonts/fontawesome-webfont.svg", "public/vendor/fonts/fontawesome-webfont.svg")
-      .copy("bower_components/font-awesome/fonts/fontawesome-webfont.ttf", "public/vendor/fonts/fontawesome-webfont.ttf")
-      .copy("bower_components/font-awesome/fonts/fontawesome-webfont.woff", "public/vendor/fonts/fontawesome-webfont.woff")
-      .copy("bower_components/font-awesome/fonts/fontawesome-webfont.woff2", "public/vendor/fonts/fontawesome-webfont.woff2")
-      .copy("bower_components/font-awesome/fonts/FontAwesome.otf", "public/vendor/fonts/FontAwesome.otf")
-      .copy("bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js", "public/vendor/js/bootstrap.min.js")
-      .copy("bower_components/jquery/dist/jquery.min.js", "public/vendor/js/jquery.min.js")
+    mix.sass(['app.scss'], 'public/assets/css')
+      .copy("bower_components/font-awesome/css/font-awesome.min.css", "public/assets/vendor/css/font-awesome.min.css")
+      .copy("bower_components/font-awesome/fonts/fontawesome-webfont.eot", "public/assets/vendor/fonts/fontawesome-webfont.eot")
+      .copy("bower_components/font-awesome/fonts/fontawesome-webfont.svg", "public/assets/vendor/fonts/fontawesome-webfont.svg")
+      .copy("bower_components/font-awesome/fonts/fontawesome-webfont.ttf", "public/assets/vendor/fonts/fontawesome-webfont.ttf")
+      .copy("bower_components/font-awesome/fonts/fontawesome-webfont.woff", "public/assets/vendor/fonts/fontawesome-webfont.woff")
+      .copy("bower_components/font-awesome/fonts/fontawesome-webfont.woff2", "public/assets/vendor/fonts/fontawesome-webfont.woff2")
+      .copy("bower_components/font-awesome/fonts/FontAwesome.otf", "public/assets/vendor/fonts/FontAwesome.otf")
+      .copy("bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js", "public/assets/vendor/js/bootstrap.min.js")
+      .copy("bower_components/jquery/dist/jquery.min.js", "public/assets/vendor/js/jquery.min.js")
       // .copy("bower_components/select2/dist/js/select2.min.js", "public/vendor/js/select2.min.js")
-      .copy("bower_components/select2/dist/js/select2.full.min.js", "public/vendor/js/select2.min.js")
-      .copy("bower_components/select2/dist/css/select2.min.css", "public/vendor/css/select2.min.css")
-      .copy("bower_components/select2-bootstrap-css/select2-bootstrap.min.css", "public/vendor/css/select2-bootstrap.min.css")
+      .copy("bower_components/select2/dist/js/select2.full.min.js", "public/assets/vendor/js/select2.min.js")
+      .copy("bower_components/select2/dist/css/select2.min.css", "public/assets/vendor/css/select2.min.css")
+      .copy("bower_components/select2-bootstrap-css/select2-bootstrap.min.css", "public/assets/vendor/css/select2-bootstrap.min.css")
       .copy(
-          'public/*',
-          '../public_html/'
+          'public/assets/*',
+          '../public_html/assets/'
         );
 });
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,9 +4,9 @@
   <title>Livrogo</title>
 
   <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-  {!! Html::style('css/app.css') !!}
-  {!! Html::style('vendor/css/font-awesome.min.css') !!}
-  {!! Html::style('vendor/css/select2.min.css') !!}
+  {!! Html::style('assets/css/app.css') !!}
+  {!! Html::style('assets/vendor/css/font-awesome.min.css') !!}
+  {!! Html::style('assets/vendor/css/select2.min.css') !!}
 
 </head>
 <body>
@@ -17,15 +17,15 @@
     <div class="row">
       <div class="col-md-12">
       @include('flash::message')
-      @include('layouts.partials._errors') 
+      @include('layouts.partials._errors')
       @yield('content')
       </div>
     </div>
   </div>
   @include('layouts.partials._footer')
-  {!! Html::script('vendor/js/jquery.min.js') !!}
-  {!! Html::script('vendor/js/bootstrap.min.js') !!}
-  {!! Html::script('vendor/js/select2.min.js') !!}
-  {!! Html::script('js/app.js') !!}
+  {!! Html::script('assets/vendor/js/jquery.min.js') !!}
+  {!! Html::script('assets/vendor/js/bootstrap.min.js') !!}
+  {!! Html::script('assets/vendor/js/select2.min.js') !!}
+  {!! Html::script('assets/js/app.js') !!}
 </body>
 </html>
