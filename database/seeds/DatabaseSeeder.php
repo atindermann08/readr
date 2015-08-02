10f1f98a5e15671a340f194c53ea6208bb2777ec
Atinderpal Singh <atindermann08@gmail.com>
2015-08-02 21:56:34 +0530

    seeder modification:

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -39,3 +39,55 @@ class DatabaseSeeder extends Seeder
         Model::reguard();
     }
 }
+
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+          'name' => 'atinder',
+          'email' => 'atinder@gmail.com',
+          'password' => bcrypt('password'),//bcrypt(str_random(10)),
+          'remember_token' => str_random(10),
+          'active' => 1,
+          ]);
+
+        DB::table('users')->insert([
+          'name' => 'jaspreet',
+          'email' => 'jaspreet@gmail.com',
+          'password' => bcrypt('password'),//bcrypt(str_random(10)),
+          'remember_token' => str_random(10),
+          'active' => 1,
+          ]);
+
+        DB::table('users')->insert([
+          'name' => 'vinay',
+          'email' => 'vinay@gmail.com',
+          'password' => bcrypt('password'),//bcrypt(str_random(10)),
+          'remember_token' => str_random(10),
+          'active' => 1,
+          ]);
+
+        DB::table('users')->insert([
+          'name' => 'amit',
+          'email' => 'amit@gmail.com',
+          'password' => bcrypt('password'),//bcrypt(str_random(10)),
+          'remember_token' => str_random(10),
+          'active' => 1,
+          ]);
+
+        DB::table('users')->insert([
+          'name' => 'maninder',
+          'email' => 'maninder@gmail.com',
+          'password' => bcrypt('password'),//bcrypt(str_random(10)),
+          'remember_token' => str_random(10),
+          'active' => 1,
+          ]);
+
+    }
