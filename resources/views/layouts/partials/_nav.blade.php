0a9baf6f68dca5da9171649c9dcf2649f3df78d4
Atinderpal Singh <atindermann08@gmail.com>
2015-08-05 14:54:07 +0530

    notification count color fix

--- resources/views/layouts/partials/_nav.blade.php
+++ resources/views/layouts/partials/_nav.blade.php
@@ -55,8 +55,9 @@
       <ul class="nav navbar-nav navbar-right">
         @if(Auth::check())
           <li class="dropdown">
-            <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+            <a href="" class="dropdown-toggle notifications" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
               <i class='fa fa-bell fa-fw @if(count($notifications)) notifications-active @endif'></i>
+              <span class="notifications-count">{{ $notifications->count() }}</span>
             </a>
             <ul class="dropdown-menu">
               @if(count($notifications))
