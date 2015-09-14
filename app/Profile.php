01ba6b295253e9953910839f818551b5bdd979e2
Atinderpal Singh <atindermann08@gmail.com>
2015-09-14 21:32:46 +0530

    thumb image null fix

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+
+  public function getImageAttribute($image)
+  {
+    if(!$image){
+      return 'assets/img/book.jpg';
+    }
+    return $image;
+  }
+
+  public function getThumbImageAttribute($image)
+  {
+      if(!$image){
+        return 'assets/img/book.jpg';
+      }
+      return $image;
+  }
+
   public function user(){
       return $this->belongsTo('\App\User');
   }
