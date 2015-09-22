e161a394e8e86c0ab06fd50ff987b12f4e041a75
Atinderpal Singh <atindermann08@gmail.com>
2015-09-22 22:37:22 +0530

    script and style tag fix

--- resources/views/layouts/partials/_scripts.blade.php
+++ resources/views/layouts/partials/_scripts.blade.php
@@ -1,6 +1,5 @@
 {!! Html::script('assets/vendor/js/jquery.min.js') !!}
 {!! Html::script('assets/vendor/js/bootstrap.min.js') !!}
 {!! Html::script('assets/vendor/js/select2.min.js') !!}
-<script src="{{ elixir('assets/js/app.js') }}">
-  
-</script>@include('partials._google_analytics')
+{!! Html::script(elixir('assets/js/app.js') !!}
+@include('partials._google_analytics')
