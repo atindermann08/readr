065e4f71a4df6403556b780b17206cc0ab9bd274
Atinderpal Singh <atindermann08@gmail.com>
2015-09-22 22:41:00 +0530

    typing correction

--- resources/views/layouts/partials/_scripts.blade.php
+++ resources/views/layouts/partials/_scripts.blade.php
@@ -1,5 +1,5 @@
 {!! Html::script('assets/vendor/js/jquery.min.js') !!}
 {!! Html::script('assets/vendor/js/bootstrap.min.js') !!}
 {!! Html::script('assets/vendor/js/select2.min.js') !!}
-{!! Html::script(elixir('assets/js/app.js') !!}
+{!! Html::script(elixir('assets/js/app.js')) !!}
 @include('partials._google_analytics')
